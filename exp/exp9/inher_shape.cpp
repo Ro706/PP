34ed15b67eb4d8261c0dbbbad921b467910ea46c
Ro706 <rohitmandal99091@gmail.com>
2023-10-12 15:16:07 +0530

    add file init

---
+++ exp/exp9/inher_shape.cpp
@@ -0,0 +1,7 @@
+//c)..Create a class named shape with a function that prints &quot;This is shape&quot;.Create another class
+//named polygon inheriting the class shape printing &quot;Polygon is a shape&quot; in method.Create two
+//rectangle and triangle having function for printing &quot;Rectangle is a polygon&quot; and &quot;Triangle is a
+//polygon&quot; respectively.Try calling the functions of this classes using the object of only one class.
+#include<iostream>
+using namespace std;
+
