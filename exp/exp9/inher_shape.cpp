d7a9a5835c973d3cdd13da801522bb22d3ddc5d1
Ro706 <rohitmandal99091@gmail.com>
2023-10-12 15:25:42 +0530

    update file

--- exp/exp9/inher_shape.cpp
+++ exp/exp9/inher_shape.cpp
@@ -4,4 +4,39 @@
 //polygon&quot; respectively.Try calling the functions of this classes using the object of only one class.
 #include<iostream>
 using namespace std;
+class Animal {
+    protected:
+        std::string name;
+    public:
+        Animal(std::string n) {
+            name = n;
+        }
+    public:
+        virtual void printName() {
+            std::cout << "Animal: " << name << std::endl;
+        }
+};
+
+class Dog : public Animal {
+    private:
+        double weight;
+    public:
+        Dog(std::string n, double w) : Animal(n) {
+            weight = w;
+        }
+    public:
+        void printName() override {
+            std::cout << "Dog: " << name << ", " << weight << " kg" << std::endl;
+        }
+};
+
+int main() {
+    Animal* a = new Animal("Leo");
+    Dog* d = new Dog("Max", 10.5);
+    a->printName();
+    d->printName();
+    delete a;
+    delete d;
+    return 0;
+}
 
